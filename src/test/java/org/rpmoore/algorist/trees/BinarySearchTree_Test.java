b3dc6d2be1fe711170b35c8a3b748b1bd1c86dc7
Ryan Moore <ryan.moore@greyrocksoft.com>
2016-08-19 00:01:23 -0600

    Adding some binary search tree tests

--- src/test/java/org/rpmoore/algorist/trees/BinarySearchTree_Test.java
+++ src/test/java/org/rpmoore/algorist/trees/BinarySearchTree_Test.java
@@ -3,6 +3,8 @@ package org.rpmoore.algorist.trees;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.junit.Assert.assertThat;
 
 public class BinarySearchTree_Test {
@@ -12,5 +14,19 @@ public class BinarySearchTree_Test {
         binarySearchTree.insert("hi");
 
         assertThat(binarySearchTree.visit(Node::getValue), is("hi"));
+    }
+
+    @Test
+    public void twoNodes() {
+        final BinarySearchTree<String> binarySearchTree = new BinarySearchTree<>();
+        binarySearchTree.insert("hi");
+        binarySearchTree.insert("hello");
+
+        assertThat(binarySearchTree.visit(Node::getValue), is("hi"));
+        assertThat(binarySearchTree.visit(node -> {
+            assertThat(node.getLeft(), is(notNullValue()));
+            assertThat(node.getRight(), is(nullValue()));
+            return node.getLeft().getValue();
+        }), is("hello"));
     }
 }
